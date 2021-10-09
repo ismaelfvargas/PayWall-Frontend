a137daff94f79b6389ac652ce8111ec53fc84d04
ismael <ismael.vargas@fundacred.org.br>
2021-10-09 19:08:15 -0300

    auth guard funcionando

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from "./auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+
+    return this.authService.estaAutenticado();
+  }
+
+}
--- src/app/pagamento/pagamento-routing.module.ts
+++ src/app/pagamento/pagamento-routing.module.ts
@@ -4,9 +4,12 @@ import {PagamentoFormComponent} from "./pagamento-form/pagamento-form.component"
 import {PagamentoListaComponent} from "./pagamento-lista/pagamento-lista.component";
 import {LayoutComponent} from "../layout/layout.component";
 import {AdiantamentosFormComponent} from "./adiantamentos-form/adiantamentos-form.component";
+import { AuthGuard } from '../auth.guard';
 
 const routes: Routes = [
-  { path: '' , component: LayoutComponent, children: [
+  { path: '' , component: LayoutComponent,
+    canActivate: [AuthGuard], children: [
+
       { path: 'pagamento-form', component: PagamentoFormComponent },
       { path: 'pagamento-listagem', component: PagamentoListaComponent },
       { path: 'adiantamentos-form', component: AdiantamentosFormComponent }
