2497400ff6ac567b707b228c532bf346f148c616
ismael <ismael.vargas@fundacred.org.br>
2021-10-11 16:02:57 -0300

    implementação de usuario logado

--- src/app/template/sidebar/sidebar.component.ts
+++ src/app/template/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../auth.service";
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogado: string;
+
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.usuarioLogado = this.authService.getUsuarioAutenticado();
   }
 
 }
